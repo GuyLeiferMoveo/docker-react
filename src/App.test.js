c5d7cf2658b72471530569248af6c512b4c69598
GuyLeiferMoveo <guyl@moveo.co.il>
2022-02-26 19:02:36 +0200

    Production Environment

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Production Environment link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Production Environment/i);
   expect(linkElement).toBeInTheDocument();
 });
