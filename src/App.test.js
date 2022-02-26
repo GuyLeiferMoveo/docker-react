c0cdbc6f625e59318bc9703cc1bf88e5a5abc450
GuyLeiferMoveo <guyl@moveo.co.il>
2022-02-26 19:03:48 +0200

    Development Environment

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Development Environment link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Development Environment/i);
   expect(linkElement).toBeInTheDocument();
 });
